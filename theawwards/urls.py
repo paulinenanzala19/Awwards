10071b9ec250ddb4557f0b27d0d39aa978572a8c
paulinenanzala19 <pauline.wafula@student.moringaschool.c0m>
2022-06-10 22:01:11 +0300

    amending the registration template

--- theawwards/urls.py
+++ theawwards/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path,include
+from . import views
+from django.urls import path,include
+from .views import *
 from django.conf import settings
 
 
 urlpatterns=[
+    path('',views.home, name='home'),
     
 ]
--- theawwards/views.py
+++ theawwards/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.http  import HttpResponse
+from .forms import *
+from.models import *
 
 # Create your views here.
+
+def home(request):
+   
+    
+    return render(request, 'index.html')
