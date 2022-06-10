bbf13bf1231e116e5497755a7ea4e65fb8bdbc49
paulinenanzala19 <pauline.wafula@student.moringaschool.c0m>
2022-06-10 19:14:05 +0300

    creating model project

--- theawwards/models.py
+++ theawwards/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Project(models.Model):
+    title = models.CharField(max_length=155)
+    url = models.URLField(max_length=255)
+    description = models.TextField(max_length=255)
