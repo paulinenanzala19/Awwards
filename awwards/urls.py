cbe547e9a63b69cf05a62e872e1316634a968bbe
paulinenanzala19 <pauline.wafula@student.moringaschool.c0m>
2022-06-10 19:17:35 +0300

    registering models in the admin

--- awwards/urls.py
+++ awwards/urls.py
@@ -19,4 +19,6 @@ from django.urls import path,include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('theawwards.urls') ),
+    path('accounts/', include('django_registration.backends.one_step.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
