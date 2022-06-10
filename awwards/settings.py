f59eaae7502b8a8fcb3a51ba81e3e460b22bc2f8
paulinenanzala19 <pauline.wafula@student.moringaschool.c0m>
2022-06-10 22:05:39 +0300

    configuring the secret key

--- awwards/settings.py
+++ awwards/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config,Csv
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-4zzn3j3n%)a1m&=t2c+46=qr)kl!u@m@g^phx-)3%v4d)n4y^y'
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
