416edeb0d4186d2085058499d335d6271382b6e5
paulinenanzala19 <pauline.wafula@student.moringaschool.c0m>
2022-06-11 14:41:14 +0300

    adding user and technologies in the model

--- theawwards/models.py
+++ theawwards/models.py
@@ -1,7 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
-class Project(models.Model):
+class Post(models.Model):
     title = models.CharField(max_length=155)
     url = models.URLField(max_length=255)
     description = models.TextField(max_length=255)
+    technologies = models.CharField(max_length=200, blank=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="posts")
+
